4fb6d3e87aa55dad348f8165d32ade7a8ad5d570
RayanHussain05 <gdhk0528@leeds.ac.uk>
2025-11-05 15:13:45 +0000

    Main menu with navigation to sub menu

---
+++ main.py
@@ -0,0 +1,193 @@
+def main_menu():
+    
+    """ displays main menu"""
+
+    print("\n*/// Menu ///*\n")
+    print("1) View To-do's")
+    print("2) Add To-do's")
+    print("3) Remove To-do's")
+    print("4) Edit To-do's") 
+    print("\n")
+    
+
+    user_input = input("Select an Option: ")
+    if user_input == "1":
+        view_menu()
+    elif user_input == "2":
+        add_menu()
+    elif user_input == "3":
+        remove_menu()
+    elif user_input == "4":
+        edit_menu()
+    else:
+        print("Choose from the Option List!!!")
+
+def view_menu():
+
+    print("\n")
+    print("1) View Monday")
+    print("2) View Tuesday")
+    print("3) View Wednesday")
+    print("4) View Thursday")
+    print("5) View Friday")
+    print("6) View Saturday")
+    print("7) View Sunday")
+    print("8) Go Back")
+    print("\n")
+
+    user_view = input("Select a day: ")
+    if user_view == "1":
+        """idk"""
+    elif user_view == "2":
+        """idk"""
+    elif user_view == "3":
+        """idk"""
+    elif user_view == "4":
+        """idk"""
+    elif user_view == "5":
+        """idk"""
+    elif user_view == "6":
+        """idk"""
+    elif user_view == "7":
+        """idk"""
+    elif user_view == "8":
+         main_menu()
+    else:
+        print("Choose from the option list!!!")
+
+def add_menu():
+    print("\n")
+    print("1) Add To Monday")
+    print("2) Add To Tuesday")
+    print("3) Add To Wednesday")
+    print("4) Add To Thursday")
+    print("5) Add To Friday")
+    print("6) Add To Saturday")
+    print("7) Add To Sunday")
+    print("8) Go Back")
+    print("\n")
+    
+    user_add = input("Select a day: ")
+    if user_add == "1":
+        """idk"""
+    elif user_add == "2":
+        """idk"""
+
+    elif user_add == "3":
+        """idk"""
+    elif user_add == "4":
+        """idk"""
+    
+    elif user_add == "5":
+        """idk"""
+
+    elif user_add == "6":
+        """idk"""
+
+    elif user_add == "7":
+        """idk"""
+
+    elif user_add == "8":
+         main_menu()
+    else:
+        print("Choose from the option list!!!")
+        
+
+
+def remove_menu():
+    print("\n")
+    print("1) Remove from Monday")
+    print("2) Remove from Tuesday")
+    print("3) Remove from Wednesday")
+    print("4) Remove from Thursday")
+    print("5) Remove from Friday")
+    print("6) Remove from Saturday")
+    print("7) Remove from Sunday")
+    print("8) Go Back")
+    print("\n")
+
+    user_remove = input("Select a day: ")
+    if user_remove == "1":
+        """idk"""
+    elif user_remove == "2":
+        """idk"""
+    elif user_remove == "3":
+        """idk"""
+    elif user_remove == "4":
+        """idk"""
+    elif user_remove == "5":
+        """idk"""
+    elif user_remove == "6":
+        """idk"""
+    elif user_remove == "7":
+        """idk"""
+    elif user_remove == "8":
+        main_menu()
+    else:
+        print("Choose from the option list!!!")
+
+def edit_menu():
+    print("\n")
+    print("1) Edit Monday")
+    print("2) Edit Tuesday")
+    print("3) Edit Wednesday")
+    print("4) Edit Thursday")
+    print("5) Edit Friday")
+    print("6) Edit Saturday")
+    print("7) Edit Sunday")
+    print("8) Go Back")
+    print("\n")
+    edit_menu = input("Select a day: ")
+    if edit_menu == "1":
+        """idk"""
+    elif edit_menu == "2":
+        """idk"""
+    elif edit_menu == "3":
+        """idk"""
+    elif edit_menu == "4":
+        """idk"""
+    elif edit_menu == "5":
+        """idk"""
+    elif edit_menu == "6":
+        """idk"""
+    elif edit_menu == "7":
+        """idk"""
+    elif edit_menu == "8":
+         main_menu()
+    else:
+        print("Choose from the option list!!!")
+
+
+    
+    
+    
+print("Choose from the option list!!!")
+
+
+
+
+    
+"""
+def add_task():
+   while True:
+        user_input = input().lower() 
+        if user_input == "add tasks":
+            print("What task would you like to add?")
+            
+        
+
+        
+        else:
+            print("\n!! Choose from the option list !!\n")
+"""
+
+main_menu()
+
+
+
+
+
+
+
+
+
